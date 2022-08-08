1f0dd483d49240033cc27aff46950cc1783dfe7b
pedro saez <pedrosaez02@gmail.com>
2022-08-07 23:13:18 -0300

    agregue h1 al html

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+let div = document.getElementById("div1");
+let h1 = document.getElementById("titulo");
 
 const productos = [
     {id:1, nombres: "Mesa Julia", precio: 50000},
@@ -15,24 +17,83 @@ const entradaUno = (entrada) => {
     if ( entrada == "") {
     prompt("Ok! Quiere conocer nuestro catalogo? SI/NO").toUpperCase();
     } else {
-    let pregunta = prompt("Hola " + entrada + " deseas conocer nuestro catalogo? SI/NO").toUpperCase();
-        if ( pregunta == "SI") {
-            prompt(mostrarCatalogo());
+    const pregunta = prompt("Hola " + entrada + " deseas conocer nuestro catalogo? SI/NO").toUpperCase();
+    console.log("Pregunta dentro de la función entradaUno : " + pregunta);
+       /*  if ( pregunta == "SI") {
+            compra = parseInt(prompt(mostrarCatalogo()));
         } else {
-            alert("Te esperamos en otro momento!");
-        }
+            alert("Ok, esperamos la proxima!");
+        } */
     }
     
-}
+    
+} 
 
 entradaUno(entrada);
 
-function mostrarCatalogo(){
-    let mostrar = "Elija su producto poniendo el número de ID! \n\n";
-    for (let catalogo of productos) {
+
+/* const muestra =function mostrarCatalogo(respuesta){
+    console.log("Aca esta la respuesta que entra en mostrarCatalogo: " + pregunta);
+    if(respuesta == "SI"){
+        let mostrar = "Elija su producto poniendo el número de ID! Escriba ´ESC´ para terminar \n\n";
+    }
+    for (const catalogo of productos) {
         mostrar += "ID: " + catalogo.id + " - " + catalogo.nombres + ": $" + catalogo.precio + "\n";  
         console.log("ID: " + catalogo.id + " - " + catalogo.nombres + ": $" + catalogo.precio + "\n");
     }
     prompt(mostrar);
+} */
+
+/* function buscarProducto(id) {
+    return productos.find((elemento)=> elemento.id == id); 
 }
+
+function agregarProducto(catalogo) {
+    carrito.push(catalogo);
+}
+
+function mostrarProductosElegidos() {
+    if(carrito.length > 0 ){
+        let mensaje = "Productos agregados: \n";
+    let totalPagar = 0;
+    
+    for (let productoCarrito of carrito) {
+
+    }
+    }
+} */
+
+// La idea es que carrito = [] se vaya llenando por un parámetro que le va a llegar desde la función entradaCarrito, el numero de ID va a idElegido y eso se pushea en la misma función 
+
+/* const carrito = [];
+const idElegido = 0
+
+function entradaCarrito(idElegido) {
+    let numeroId = idElegido
+    carrito.push(numeroId)
+
+}
+ */
+
+
+// Aca no llega el prompt pregunta
+const una = function(pregunta) {
+    console.log(pregunta);
+    if (pregunta == "SI"){
+        let mostrar = "Elija su producto poniendo el número de ID! Escriba ´ESC´ para terminar \n\n";
+        for (const catalogo of productos) {
+            mostrar += "ID: " + catalogo.id + " - " + catalogo.nombres + ": $" + catalogo.precio + "\n";  
+            console.log("ID: " + catalogo.id + " - " + catalogo.nombres + ": $" + catalogo.precio + "\n");
+        }    
+    } else {
+        prompt("Ok, te esperamos la proxima!")
+    }
+    
+}
+
+
+
+
+
+
 
