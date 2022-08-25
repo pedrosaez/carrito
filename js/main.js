d7857eb1e4268452668b26750c3e6e2499683b51
pedro saez <pedrosaez02@gmail.com>
2022-08-25 12:29:36 -0300

    render boton carrito pero no funciona todavia

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
 const muebles = [
-    {id:1, nombres: "Mesa Julia", precio: 50000, imagen:"mesaJulia.jpg"},
-    {id:2, nombres: "Rack Tv", precio: 45000, imagen:"rack.jpg"},
-    {id:3, nombres: "Sofá Barcelona", precio: 85000, imagen:"sillon2.jpg"},
-    {id:4, nombres: "Sofá Chester", precio: 105000, imagen:"sillon5.jpg"},
-    {id:5, nombres: "Mesa Ratona", precio: 20000, imagen:"mesaRatona.jpg"},   
-    {id:5, nombres: "Vanitory", precio: 30000, imagen:"vanitory.jpg"}   
+    { id: 1, nombres: "Mesa Julia", precio: 50000, imagen: "mesaJulia.jpg" },
+    { id: 2, nombres: "Rack Tv", precio: 45000, imagen: "rack.jpg" },
+    { id: 3, nombres: "Sofá Barcelona", precio: 85000, imagen: "sillon2.jpg" },
+    { id: 4, nombres: "Sofá Chester", precio: 105000, imagen: "sillon5.jpg" },
+    { id: 5, nombres: "Mesa Ratona", precio: 20000, imagen: "mesaRatona.jpg" },
+    { id: 5, nombres: "Vanitory", precio: 30000, imagen: "vanitory.jpg" },
 ];
+    const mueblesCarrito = [];
 
 function mueblesEnLs(muebles) {
     localStorage.setItem("muebles", JSON.stringify(muebles));
@@ -15,8 +16,6 @@ function cargarMueblesLs() {
     return JSON.parse(localStorage.getItem("muebles")) || [];
 }
 
-mueblesEnLs(muebles);
-
 function mueblesEnCarrito(muebles) {
     localStorage.setItem("mueblesCarrito", JSON.stringify(muebles));
 }
@@ -25,71 +24,53 @@ function cargarMueblesCarrito() {
     return JSON.parse(localStorage.getItem("mueblesCarrito")) || [];
 }
 
-function buscarMueble(id){
-    const muebles = cargarMueblesLs();
-    return muebles.find(item => item.id === id);
+function buscarMueble(id) {
+const muebles = cargarMueblesLs();
+    return muebles.find((item) => item.id === id);
 }
 
-function agregarMuebles(id){
-    const mueblesCarrito = mueblesEnCarrito();
+// Jaider no me esta funcionando todavía, voy a tener que verme todas las clases de nuevo para entregar el proyecto final jajja
+
+function agregarMuebles(id) {
     const mueble = buscarMueble(id);
-    mueblesCarrito.push(mueble);
-    mueblesEnCarrito(mueblesCarrito);
-}
-
-function actualizarCarrito(){
-    const mueblesCarrito = mueblesEnCarrito();
-    let total = mueblesCarrito.length;
+    mueblesEnCarrito([...cargarMueblesCarrito(), mueble]);
     
-
 }
 
 
+function actualizarCarrito() {
+const mueblesCarrito = mueblesEnCarrito();
+let total = mueblesCarrito.length;
+let contenido = `<button type="button" class="btn btn-primary position-relative">Carrito
+<span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">${total}</span>
+</button>`;
+document.getElementById("botonCarrito").innerHTML = contenido;
+}
 
+function vistaMuebles() {
+const muebles = cargarMueblesLs();
+let catalogo = "";
 
-function vistaMuebles(){
-    const muebles = cargarMueblesLs();
-    let catalogo = "";
-
-    muebles.forEach((mueble) => {
-        catalogo += `<div class="col-md-4 row justify-content-center">
-                    <div class="card text-center border border-0" style="width: 18rem;">
-                    <img src="./img/${mueble.imagen}" class="card-img-top" alt="${mueble.nombres}">
-                    <div class="card-body">
-                    <h5 class="card-title">${mueble.nombres}</h5>
-                    <p class="card-text">$${mueble.precio}</p>
-                    <a href="#" class="btn btn-outline-dark" title="Agregar" onclick="agregarMueble(${mueble.id})">Agregar al carrito</a>
-                    </div>
-                </div>
-            </div>`;
+muebles.forEach((mueble) => {
+catalogo += `<div class="col-md-3 row justify-content-center">
+            <div class="card text-center border border-0" style="width: 18rem;">
+            <img src="./img/${mueble.imagen}" class="card-img-top" alt="${mueble.nombres}">
+            <div class="card-body">
+            <h5 class="card-title">${mueble.nombres}</h5>
+            <p class="card-text">$${mueble.precio}</p>
+            <a href="#" class="btn btn-outline-dark" title="Agregar" onclick="agregarMuebles(${mueble.id})">Agregar al carrito</a>
+            </div>
+            </div>
+        </div>`;
     });
 
-    document.getElementById("muebles").innerHTML = catalogo;
+document.getElementById("muebles").innerHTML = catalogo;
 }
 
+
+mueblesEnLs(muebles);
 vistaMuebles();
-
-
-
-
-
-
-
-
-
-/* for (const mueble of muebles) {
-    let contenedor = document.createElement("div");
-    contenedor.className= "col-md-4";  
-    contenedor.innerHTML=   `<div class="col-md-4 card text-center border border-0" style="width: 18rem;">
-                            <img src="./img/${mueble.imagen}" class="card-img-top" alt="...">
-                            <div class="card-body">
-                            <h5 class="card-title">${mueble.nombres}</h5>
-                            <p class="card-text">$${mueble.precio}</p>
-                            <button id="agregar" href="#" class="btn btn-dark">Agregar al carrito</button>
-                            </div>`;
-    document.getElementById("muebles").appendChild(contenedor);                        
-
-} */
+actualizarCarrito();
 
 
 
